73a9ddcbbd9ab7f64b07fd3668f63fcdf1ef3b9e
hiroshiuk1991 <hiroshis91@icloud.com>
2020-02-17 15:49:47 +0000

    cors added

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,10 @@ Rails.application.routes.draw do
   resources :portfolios
   resources :investors
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  post '/login', to: 'investors#login'
+  get '/validate', to: 'investors#validate'
+  get '/fetchinvestorporfolio', to: 'fetchinvestorportfolio'
+
+
 end
