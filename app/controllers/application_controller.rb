b31c61e6328f00ed9fad76b0fb5445e28a7466b7
hiroshiuk1991 <hiroshis91@icloud.com>
2020-02-17 16:14:56 +0000

    added investor controller methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,27 @@
 class ApplicationController < ActionController::API
+
+    def get_current_investor
+        id = decoded_token['id']
+        Investor.find_by(id: id)
+    end 
+
+    def decoded_token
+        begin
+            JWT.decode(token, secret).first
+        rescue 
+            {}
+            end 
+        end
+
+    def token 
+        request.headers['Authorization']
+    end 
+
+    def issue_token(data)
+        JWT.encode(data, secret)
+    end 
+
+    def secret
+        ENV['MY_SECRET']
+    end 
 end
--- app/controllers/investors_controller.rb
+++ app/controllers/investors_controller.rb
@@ -2,4 +2,61 @@ class InvestorsController < ApplicationController
 has_one :portfolio 
 has_many :stocks, through: :portfolio
 
+
+    def index
+        investor =Investor.all 
+        render json: investor
+    end 
+
+    def show 
+        investor = Investor.find(params[:id])
+        render json: {id: investor.id, name: investor.name, email: investor.email, password: investor.password}
+    end 
+
+    def create
+        investor = Investor.create(investor_params)
+        render json: {investor_name: investor.name, investor_id: investor.id, token:issue_token({id: investor.id})
+    end 
+
+    def destroy
+        Investor.destroy(params[:id])
+    end 
+
+    def update
+        investor = Investor.find(params[:id])
+    end 
+
+    def fetchinvestorportfolio
+        investor = get_current_investor
+        render json: investor.portfolio
+    end 
+
+
+    def login
+        investor = Investor.find_by(name: params[:investor][:name])
+
+        if investor and investor.authenticate(params[:investor][:password])
+
+            render json: {investor_name: investor.name, investor_id: investor.id, token:issue_token({id: investor.id})}
+        else 
+
+            render json: {error: 'Investor name/password invalid'}, status: 403
+        end 
+    end 
+
+    def validate 
+        investor = get_current_investor
+        if investor
+            render json:{investor: investor.name, token:issue_token({id: investor.id})}
+        else
+            render json: {error: 'Not Authorized'}, status: 401
+        end 
+    end 
+
+    private 
+
+    def investor_params
+        params.require(:investor).permit(:username, :password)
+    end 
+
 end
