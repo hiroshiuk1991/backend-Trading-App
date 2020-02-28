d2ad9d61f16c58fe3157ad462a4ecc8f8b33a521
hiroshiuk1991 <hiroshis91@icloud.com>
2020-02-28 14:16:37 +0000

    updates route

--- app/controllers/investors_controller.rb
+++ app/controllers/investors_controller.rb
@@ -12,7 +12,7 @@ class InvestorsController < ApplicationController
 
     def create
         investor = Investor.create(investor_params)
-        render json: {investor_name: investor.name, investor_id: investor.id, token:issue_token({id: investor.id})}
+        render json: investor_hash(investor)
     end 
 
     def destroy
@@ -37,7 +37,7 @@ class InvestorsController < ApplicationController
 
         if investor and investor.authenticate(params[:investor][:password])
 
-            render json: {investor_name: investor.name, investor_id: investor.id, token:issue_token({id: investor.id})}
+            render json: investor_hash(investor)
         else 
 
             render json: {error: 'Investor name/password invalid'}, status: 403
@@ -47,13 +47,17 @@ class InvestorsController < ApplicationController
     def validate 
         investor = get_current_investor
         if investor
-            render json:{name: investor.name, investorId: investor.id, token:issue_token({id: investor.id})}
+            render json: investor_hash(investor)
         else
             render json: {error: 'Not Authorized'}, status: 401
         end 
     end 
 
     private 
+
+    def investor_hash(investor)
+        {name: investor.name, investorId: investor.id, token:issue_token({id: investor.id})}
+    end
 
     def investor_params
         params.require(:investor).permit(:name, :password)
--- app/models/investor.rb
+++ app/models/investor.rb
@@ -1,9 +1,8 @@
 class Investor < ApplicationRecord
-
+validates :name, presence: true
     has_secure_password 
 
     has_many :quiz_score
-
 
     has_one :portfolio 
     has_many :stocks, through: :portfolio  
